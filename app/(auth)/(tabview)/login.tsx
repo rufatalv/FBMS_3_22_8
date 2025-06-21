8e1a4a3d7726565c3cde7bed961bc07532c8d763
Rufat Aliyev <rufat845@gmail.com>
2025-06-21 10:03:46 +0400

    Add authentication layout and screens with navigation

---
+++ app/(auth)/(tabview)/login.tsx
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Image } from "expo-image";
+
+const Login = () => {
+  return (
+    <View className="flex-1 items-center justify-center bg-red-500">
+      <Text>Login</Text>
+    </View>
+  );
+};
+
+export default Login;
+
+const styles = StyleSheet.create({});
